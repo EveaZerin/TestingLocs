450ca6ca06db6d5fa62d4f6451bb0c760031b735
EveaZerin <evea.zerin@gmail.com>
2023-06-04 14:25:26 +0600

    react app with i18next

---
+++ frontend/my-app/src/i18n.js
@@ -0,0 +1,29 @@
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import { initReactI18next } from 'react-i18next';
+import de from './locales/de/translation.json';
+import en from './locales/de/translation.json';
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    debug: true,
+    fallbackLng: 'en',
+    returnEmptyString: false,
+    interpolation: {
+      escapeValue: false, // not needed for react as it escapes by default
+    },
+    supportedLngs: ['en', 'de'],
+    nonExplicitSupportedLngs: true,
+    resources: {
+      en: {
+        translation: en,
+      },
+      de: {
+        translation: de,
+      },
+    },
+  });
+
+export default i18n;
