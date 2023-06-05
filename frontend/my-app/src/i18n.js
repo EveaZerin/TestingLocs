e628746ce07d4021ec86203a014548ed262792ea
EveaZerin <evea.zerin@gmail.com>
2023-06-05 12:55:11 +0600

    added .yml to backend

--- frontend/my-app/src/i18n.js
+++ frontend/my-app/src/i18n.js
@@ -2,7 +2,7 @@ import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 import de from './locales/de.json';
-import en from './locales/de.json';
+import en from './locales/en.json';
 
 i18n
   .use(LanguageDetector)
--- frontend/my-app/src/locales/it.js
+++
@@ -1,7 +0,0 @@
-export var translations = {
-  "plural_key": {
-      "one": "it-One user online.",
-      "other": "it-%i users online."
-  },
-  "simple_key": "it-Hi, I am a key!"
-};
