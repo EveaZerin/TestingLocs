2734410f674788efc315ebf318ec17d31d96ea4e
EveaZerin <evea.zerin@gmail.com>
2023-06-05 10:32:48 +0600

    testing .js files

---
+++ frontend/my-app/src/locales/fr.js
@@ -0,0 +1,7 @@
+export var translations = {
+  "plural_key": {
+      "one": "fr-One user online.",
+      "other": "fr-%i users online."
+  },
+  "simple_key": "fr-Hi, I am a key!"
+};
---
+++ frontend/my-app/src/locales/it.js
@@ -0,0 +1,7 @@
+export var translations = {
+  "plural_key": {
+      "one": "it-One user online.",
+      "other": "it-%i users online."
+  },
+  "simple_key": "it-Hi, I am a key!"
+};
